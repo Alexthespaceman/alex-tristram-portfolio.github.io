a62d5e0663f73657c7c1021269fed3835d99252a
Alex Tristram <allex.tristram.gmail.com>
2021-05-05 23:11:33 +0100

    add more styling

--- alexs-portfolio/src/components/MyWork.jsx
+++ alexs-portfolio/src/components/MyWork.jsx
@@ -1,10 +1,28 @@
 import React from "react";
+// import image1 from "../images/images/portfolio-item11.jpg";
 
 function MyWork(props) {
   return (
     <div>
       <section className="work">
         <p className="title3">...Some of my best work</p>
+        <div className="flex-box-container3">
+          <div className="long div div1">
+            <div className="div">
+              <a>{/* <img scr={image1} alt="portfolio" /> */}</a>
+            </div>
+          </div>
+          <div className="long div2">
+            <div className="div">
+              <a>{/* <img /> */}</a>
+            </div>
+          </div>
+          <div className="long div3">
+            <div className="div">
+              <a>{/* <img /> */}</a>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
