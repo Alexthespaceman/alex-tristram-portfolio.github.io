7b504527dff4146619112f3a698b44c1166b2443
Alex Tristram <allex.tristram.gmail.com>
2021-05-05 23:14:35 +0100

    add more styling

--- alexs-portfolio/src/components/MyWork.jsx
+++ alexs-portfolio/src/components/MyWork.jsx
@@ -8,17 +8,17 @@ function MyWork(props) {
         <p className="title3">...Some of my best work</p>
         <div className="flex-box-container3">
           <div className="long div div1">
-            <div className="div">
+            <div className="div divide">
               <a>{/* <img scr={image1} alt="portfolio" /> */}</a>
             </div>
           </div>
           <div className="long div2">
-            <div className="div">
+            <div className="div divide">
               <a>{/* <img /> */}</a>
             </div>
           </div>
           <div className="long div3">
-            <div className="div">
+            <div className="div divide">
               <a>{/* <img /> */}</a>
             </div>
           </div>
